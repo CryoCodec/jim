5ffaa9af3286e1f1eca1fa96b4d6668e54615c95
Lukas Buchner <lukas.buchner@partner.bmw.de>
2021-10-25 09:50:03 +0200

    set version to 1.0.0-rc2

--- cmd/version.go
+++ cmd/version.go
@@ -11,7 +11,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Prints jim's version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("jim version 1.0.0-rc")
+		fmt.Println("jim version 1.0.0-rc2")
 	},
 }
 
